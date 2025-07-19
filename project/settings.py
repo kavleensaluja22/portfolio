a4395f00793c0cfe23e26e57bcddc2fa8b066460
kavleen <kavleensaluja22@gmail.com>
2025-07-19 13:43:00 +0530

    allowed

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q3fw_8s4x=7n)e+az#zwd!b3f5tt!%dq32=zzrj!hqi&(%ci(0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['portfolio-7wjz.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
